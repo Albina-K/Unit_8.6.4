f928719f64c25f1f895f1842f6f8702c9504f922
Albina <kamalieva.albish@mail.ru>
2024-04-09 15:41:13 +0300

    Добавьте файлы проекта.

---
+++ Unit_8.6.4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Unit_8_6_4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string filePath = @"C:\\User\\Luft\\SkillFactoryNew\\students.dat";
+            // var fileInfo = new FileInfo(filePath);
+            string Name;
+            string Group;
+            long DateOfBirth;
+           // DataTime DateOfBirth;
+            decimal Grade;
+            if (File.Exists(filePath)) 
+            {
+                //string stringValue;
+                
+                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open))) 
+                {
+                    Name = reader.ReadString();
+                    Group = reader.ReadString(); 
+                    DateOfBirth = reader.ReadInt64(); ;
+                    // DataTime DateOfBirth;
+                    decimal Grade;
+                }
+
+                Console.WriteLine(stringValue);
+              /*  try
+                {
+                    string filePath2 = @"C:\\User\\Luft\\SkillFactoryNew\\student.dat";
+                    var fileInfo2 = new FileInfo(filePath2);
+
+                    fileInfo2.Delete();
+
+                    fileInfo.CopyTo(filePath2);
+                    Console.WriteLine($"{filePath} скопирован {filePath2}");
+
+                    fileInfo.Delete();
+                    Console.WriteLine($"{filePath} удален");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Ошибка: {e}");
+                }*/
+            }
+        }
+    }
+}
